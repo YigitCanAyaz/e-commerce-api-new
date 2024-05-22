0fb26f56cee0c9fd34307da403e9b41bf1643713
YigitCanAyaz <yigitcanayaz2000@gmail.com>
2024-05-22 23:24:41 +0300

    Generic Repository Pattern Implementation

---
+++ Presentation/ECommerceAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using ECommerceAPI.Application.Repositories;
+using ECommerceAPI.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductWriteRepository _productWriteRepsoitory;
+        private readonly IProductReadRepository _productReadRepsoitory;
+
+        public ProductsController(IProductWriteRepository productWriteRepsoitory, IProductReadRepository productReadRepsoitory)
+        {
+            _productWriteRepsoitory = productWriteRepsoitory;
+            _productReadRepsoitory = productReadRepsoitory;
+        }
+
+        [HttpGet]
+        public async Task Get()
+        {
+            await _productWriteRepsoitory.AddRangeAsync(new()
+             {
+                new() {Id = Guid.NewGuid(), Name = "Product 1", Price = 100, CreatedDate = DateTime.UtcNow, Stock = 10},
+                new() {Id = Guid.NewGuid(), Name = "Product 2", Price = 200, CreatedDate = DateTime.UtcNow, Stock = 20},
+                new() {Id = Guid.NewGuid(), Name = "Product 3", Price = 300, CreatedDate = DateTime.UtcNow, Stock = 30},
+            });
+
+            await _productWriteRepsoitory.SaveAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            Product product = await _productReadRepsoitory.GetByIdAsync(id);
+            return Ok(product);
+        }
+    }
+}
